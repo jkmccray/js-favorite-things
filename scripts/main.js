133e04827a728f7b2df5e05ed5041343db920794
Jacquelyn M <jacquelynkmccray@gmail.com>
2019-07-23 16:09:53 -0500

    started practice exercise on favorite things. got inputs and save btn to display in the dom

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+const favoriteThings = document.querySelector(".favoriteThings")
+
+const elFactory = (elType, attributesObj, txt) => {
+  const newEl = document.createElement(elType)
+  for (let attribute in attributesObj) {
+    if (attribute === "classes") {
+      const classesArray = attributesObj[attribute]
+      classesArray.forEach(cls => {
+        newEl.classList.add(cls)
+      });
+    }
+    else {
+      newEl[attribute] = attributesObj[attribute]
+    }
+  }
+  newEl.textContent = txt || null
+  return newEl
+}
+
+const nameFieldset = elFactory("fieldset")
+const nameLabel = elFactory("label", { for: "faveName" }, "Name a Favorite Thing: ")
+const nameInput = elFactory("input", { type: "text", id: "faveName", placeholder: "Enter the name of a thing", autofocus: null })
+const storeFieldset = elFactory("fieldset")
+const storeLabel = elFactory("label", { for: "store" }, "Where to Buy ")
+const storeInput = elFactory("input", { type: "text", id: "store", placeholder: "Store Name" })
+const saveBtn = elFactory("button", { id: "saveEntry" }, "Save to Wish List")
+
+favoriteThings.appendChild(nameFieldset)
+nameFieldset.appendChild(nameLabel)
+nameFieldset.appendChild(nameInput)
+favoriteThings.appendChild(storeFieldset)
+storeFieldset.appendChild(storeLabel)
+storeFieldset.appendChild(storeInput)
+favoriteThings.appendChild(saveBtn)
+
+// saveBtn.addEventListener("click", () => {
+//   const 
+// })
